0b60fd52bf24a769dabc5656c630e859bc29b888
Audrey Taylor-Akwenye <audrey@Audreys-MacBook-Air.local>
2018-07-19 18:06:05 -0500

    first push

---
+++ 01week/datatypes.js
@@ -0,0 +1,49 @@
+// use const d to get the date 
+const d = new Date();
+alert (d); 
+// Number to string
+const num = 47
+console.log(num);
+console.log(num.toString);
+
+// String to Number
+
+const text = '42px';
+const integer = parseInt(text, 10);
+
+// Data types
+
+/* Bolean  Check if 7 is greater than 8 */
+7 > 8; // returns false
+
+/* Check if a variable is equal to a string */
+var color = "Blue";
+
+color === "Blue"; // returns true
+
+const x;               //  x is undefined
+x = 5;                //  x is a Number
+x = "John";          //  x is a String
+
+const SumTwoNumbers=(num1, num2)=>{
+  return num1 + num2
+};
+SumTwoNumbers(2, 2);
+
+const aﾠ = 1;
+const a = 2;
+if(aﾠ==1 && a== 2) {
+    console.log("true")
+}
+
+var aﾠ = 1;
+var a = 2;
+if(aﾠ==1 && a!= 2) {
+    console.log("true")
+}
+var aﾠ = 1;
+var a = 2;
+
+if(aﾠ!=1 && a!= 2) {
+    console.log("not true")
+}
